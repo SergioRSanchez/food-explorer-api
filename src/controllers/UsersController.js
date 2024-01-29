81072814adcbbc736539f11b8a352be5356e34de
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-29 12:24:28 -0300

    feat: created UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -4,3 +4,65 @@ const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 const UserRepository = require("../repositories/UserRepository");
 const UserCreateService = require("../services/UserCreateService");
+
+class UserController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
+
+    const userRepository = new UserRepository();
+    const userCreateService = new UserCreateService(userRepository);
+
+    await userCreateService.execute({ name, email, password });
+
+    return response.status(201).json();
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const user_id = request.user.id;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE od = (?)", [user_id]);
+
+    if (!user) {
+      throw new AppError("User not found");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("E-mail already in use");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError("You need to inform the old password to set a new password");
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("Old password does not match");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?
+    `, [user.name, user.email, user.password, user_id]);
+
+    return response.status(200).json();
+  }
+}
+
+
+module.exports = UserController;
