a8e7987f6ba262d3f8c851c6659514295a6c12ed
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-28 21:54:26 -0300

    feat: created UserCreateService

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,3 +3,4 @@ const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 const UserRepository = require("../repositories/UserRepository");
+const UserCreateService = require("../services/UserCreateService");
