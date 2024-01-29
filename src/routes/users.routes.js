4f8395507b3884ea1e9f0611eb6220558cb1d0e8
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-29 12:34:13 -0300

    const: knex config

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,3 +3,4 @@ const multer = require("multer");
 
 const uploadConfig = require("../configs/upload");
 const UserController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
