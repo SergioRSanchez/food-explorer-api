105d86259081d25a6e6087ad08651a42c73db149
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-29 16:28:36 -0300

    feat: created sessionRoutes and sessionController

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
 const mealsRouter = require("./meals.routes");
+const sessionsRoutes = require("./sessions.routes");
 
 const routes = Router();
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRoutes);
 routes.use('/meals', mealsRouter);
 
 
