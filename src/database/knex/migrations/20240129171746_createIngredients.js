d8820e1ea9df314fd58f02160d34408f3d652040
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-03-04 18:09:09 -0300

    feat: fixed delete on cascade

--- src/database/knex/migrations/20240129171746_createIngredients.js
+++ src/database/knex/migrations/20240129171746_createIngredients.js
@@ -2,7 +2,7 @@ exports.up = knex => knex.schema.createTable("ingredients", table => {
   table.increments("id");
   table.text("name").notNullable();
 
-  table.integer("meal_id").references("id").inTable("meals");
+  table.integer("meal_id").references("id").inTable("meals").onDelete("CASCADE");
 
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
